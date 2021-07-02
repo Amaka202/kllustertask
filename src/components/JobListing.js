7cda126ae962f37fdfd264349c62aab83f8d17b9
Umeh Amaka <amakaumeh01@gmail.com>
2021-07-02 20:21:22 +0100

    build the ui

---
+++ src/components/JobListing.js
@@ -0,0 +1,189 @@
+import React from 'react';
+import iterateLogo from '../images/iterate-logo.jpeg';
+import '../styles/joblisting.css';
+
+function JobListing() {
+    return (
+        <div className='job-listing-container'>
+            <div className='card'>
+                <div className='job-type'>
+                    <div className='company-logo-div'>
+                        <img src={iterateLogo} alt='company logo' />
+                    </div>
+                    <div className='job-listing-div'>
+                        <p>Bloomberge
+                        <span className='new-badge'>NEW</span>
+                        </p>
+                        <p>Junior Developer
+                            
+                            <span className='timestamp'> 2 mins ago</span>
+                        </p>
+                    </div>
+                </div>
+                <div className='job-tag'>
+                    <p>Ruby</p>
+                    <p>Javascript</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                </div>
+            </div>
+            <div className='card'>
+                <div className='job-type'>
+                    <div className='company-logo-div'>
+                        <img src={iterateLogo} alt='company logo' />
+                    </div>
+                    <div className='job-listing-div'>
+                        <p>Bloomberge
+                        <span className='new-badge'>NEW</span>
+                        </p>
+                        <p>Junior Developer
+                            
+                            <span className='timestamp'> 2 mins ago</span>
+                        </p>
+                    </div>
+                </div>
+                <div className='job-tag'>
+                    <p>Ruby</p>
+                    <p>Javascript</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                </div>
+            </div>
+            <div className='card'>
+                <div className='job-type'>
+                    <div className='company-logo-div'>
+                        <img src={iterateLogo} alt='company logo' />
+                    </div>
+                    <div className='job-listing-div'>
+                        <p>Bloomberge
+                        <span className='new-badge'>NEW</span>
+                        </p>
+                        <p>Junior Developer
+                            
+                            <span className='timestamp'> 2 mins ago</span>
+                        </p>
+                    </div>
+                </div>
+                <div className='job-tag'>
+                    <p>Ruby</p>
+                    <p>Javascript</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                </div>
+            </div>
+            <div className='card'>
+                <div className='job-type'>
+                    <div className='company-logo-div'>
+                        <img src={iterateLogo} alt='company logo' />
+                    </div>
+                    <div className='job-listing-div'>
+                        <p>Bloomberge
+                        <span className='new-badge'>NEW</span>
+                        </p>
+                        <p>Junior Developer
+                            
+                            <span className='timestamp'> 2 mins ago</span>
+                        </p>
+                    </div>
+                </div>
+                <div className='job-tag'>
+                    <p>Ruby</p>
+                    <p>Javascript</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                </div>
+            </div>
+            <div className='card'>
+                <div className='job-type'>
+                    <div className='company-logo-div'>
+                        <img src={iterateLogo} alt='company logo' />
+                    </div>
+                    <div className='job-listing-div'>
+                        <p>Bloomberge
+                        <span className='new-badge'>NEW</span>
+                        </p>
+                        <p>Junior Developer
+                            
+                            <span className='timestamp'> 2 mins ago</span>
+                        </p>
+                    </div>
+                </div>
+                <div className='job-tag'>
+                    <p>Ruby</p>
+                    <p>Javascript</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                </div>
+            </div>
+            <div className='card'>
+                <div className='job-type'>
+                    <div className='company-logo-div'>
+                        <img src={iterateLogo} alt='company logo' />
+                    </div>
+                    <div className='job-listing-div'>
+                        <p>Bloomberge
+                        <span className='new-badge'>NEW</span>
+                        </p>
+                        <p>Junior Developer
+                            
+                            <span className='timestamp'> 2 mins ago</span>
+                        </p>
+                    </div>
+                </div>
+                <div className='job-tag'>
+                    <p>Ruby</p>
+                    <p>Javascript</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                </div>
+            </div>
+            <div className='card'>
+                <div className='job-type'>
+                    <div className='company-logo-div'>
+                        <img src={iterateLogo} alt='company logo' />
+                    </div>
+                    <div className='job-listing-div'>
+                        <p>Bloomberge
+                        <span className='new-badge'>NEW</span>
+                        </p>
+                        <p>Junior Developer
+                            
+                            <span className='timestamp'> 2 mins ago</span>
+                        </p>
+                    </div>
+                </div>
+                <div className='job-tag'>
+                    <p>Ruby</p>
+                    <p>Javascript</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                </div>
+            </div>
+            <div className='card'>
+                <div className='job-type'>
+                    <div className='company-logo-div'>
+                        <img src={iterateLogo} alt='company logo' />
+                    </div>
+                    <div className='job-listing-div'>
+                        <p>Bloomberge
+                        <span className='new-badge'>NEW</span>
+                        </p>
+                        <p>Junior Developer
+                            
+                            <span className='timestamp'> 2 mins ago</span>
+                        </p>
+                    </div>
+                </div>
+                <div className='job-tag'>
+                    <p>Ruby</p>
+                    <p>Javascript</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default JobListing;
