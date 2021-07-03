51109562e0ef64276c33932c5388d528e2966ca1
Umeh Amaka <amakaumeh01@gmail.com>
2021-07-03 14:20:20 +0100

    make the code more readable

--- src/components/JobListing.js
+++ src/components/JobListing.js
@@ -4,32 +4,37 @@ import '../styles/joblisting.css';
 function JobListing({ data }) {
     return (
         <div >
-            {data.map(job => {
-                return (
-                    <div className='card' key={job.id}>
-                        <div className='job-type'>
-                            <div className='company-logo-div'>
-                                <img src={job.logo} alt='company logo' />
+            {data.length > 0
+                ?
+                data.map(job => {
+                    return (
+                        <div className='card' key={job.id}>
+                            <div className='job-type'>
+                                <div className='company-logo-div'>
+                                    <img src={job.logo} alt='company logo' />
+                                </div>
+                                <div className='job-listing-div'>
+                                    <p>{job.companyName}
+                                        {job.newPost && <span className='new-badge'>NEW</span>}
+                                    </p>
+                                    <p>{job.jobType}
+                                        <span className='timestamp'> {job.timestamp}</span>
+                                    </p>
+                                </div>
                             </div>
-                            <div className='job-listing-div'>
-                                <p>{job.companyName}
-                                    {job.newPost && <span className='new-badge'>NEW</span>}
-                                </p>
-                                <p>{job.jobType}
-                                    <span className='timestamp'> {job.timestamp}</span>
-                                </p>
+                            <div className='job-tag'>
+                                {job.jogTags.map((tag, index) => {
+                                    return (
+                                        <p key={index}>{tag}</p>
+                                    )
+                                })}
                             </div>
                         </div>
-                        <div className='job-tag'>
-                            {job.jogTags.map((tag, index) => {
-                                return (
-                                    <p key={index}>{tag}</p>
-                                )
-                            })}
-                        </div>
-                    </div>
-                )
-            })}
+                    )
+                })
+                :
+                <p style={{ textAlign: 'center' }}>No Jobs to display</p>
+            }
         </div>
     )
 }
